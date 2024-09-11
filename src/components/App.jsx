f42c8c6a9610d9c35b59f7d25ae3bf3803b05b15
NadaJrad <nadajradbenaicha@gmail.com>
2024-09-11 13:54:03 +0200

    mouseover added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,14 +4,26 @@ import { useState } from "react";
 
 function App() {
   const [handelingText, setHandelingText] = useState("Hello");
+  const [isMousedOver, setMouseOver] = useState(false);
+
   function handelClick() {
     setHandelingText("submitted");
+  }
+
+  function handelMouseOver() {
+    setMouseOver(true);
   }
   return (
     <div className="container">
       <h1>{handelingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button onClick={handelClick}>Submit</button>
+      <button
+        style={{ backgroundColor: isMousedOver ? "black" : "white" }}
+        onClick={handelClick}
+        onMouseOver={handelMouseOver}
+      >
+        Submit
+      </button>
     </div>
   );
 }
